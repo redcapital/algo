ad0bf0081753bca44a7d8b5c09c795e4af050c95
Galymzhan <kozhayev@gmail.com>
2014-03-10 10:11:41 +0600

    solved timus 2001

---
+++ timus/2001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int a0, b0, a1, b1, a2, b2;
+  cin >> a0 >> b0 >> a1 >> b1 >> a2 >> b2;
+  cout << (a0 - a2) << " " << (b0 - b1) << endl;
+  return 0;
+}
